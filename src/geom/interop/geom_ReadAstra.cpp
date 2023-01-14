ca12f266c4b2f68731829082b152457a34bcd2f0
Quaoar <sergey.slyadnev@gmail.com>
2023-01-14 22:08:00 +0300

    Populate the two-dimensional array of points from Astra surface definition.

--- src/geom/interop/geom_ReadAstra.cpp
+++ src/geom/interop/geom_ReadAstra.cpp
@@ -38,11 +38,13 @@ using namespace mobius;
 
 //-----------------------------------------------------------------------------
 
-#define Astra_CurveArraySize 7
-
-//-----------------------------------------------------------------------------
+#define Astra_ArrayHeaderSize 7
+#define Astra_CurvePointSize  7
+#define Astra_SurfPointSize   14
 
 namespace {
+
+  //-----------------------------------------------------------------------------
 
   //! Curve DTO.
   struct t_curveData
@@ -128,19 +130,125 @@ namespace {
     }
   };
 
+  //-----------------------------------------------------------------------------
+
+  //! Surface DTO.
+  struct t_surfData
+  {
+    //! Surface point.
+    struct t_pt
+    {
+      double x, y, z, Pu_x, Pu_y, Pu_z, Pv_x, Pv_y, Pv_z, Puv_x, Puv_y, Puv_z, u, v;
+
+      //! Default ctor.
+      t_pt() : x     (0.), y     (0.), z     (0.),
+               Pu_x  (0.), Pu_y  (0.), Pu_z  (0.),
+               Pv_x  (0.), Pv_y  (0.), Pv_z  (0.),
+               Puv_x (0.), Puv_y (0.), Puv_z (0.),
+               u     (0.), v     (0.)
+      {}
+    };
+
+    //! Ctor.
+    t_surfData() : ptSerial(0) {}
+
+    //! Adds a surface point from the passed tokens.
+    void AddPoint(const std::vector<std::string>& tokens)
+    {
+      this->ptSerial++;
+
+      const int j = (this->ptSerial - 1) / this->nptsU;
+      const int i = (this->ptSerial - 1) % this->nptsU;
+
+      t_pt pt;
+      pt.x     = core::str::to_number<double>(tokens[0]);
+      pt.y     = core::str::to_number<double>(tokens[1]);
+      pt.z     = core::str::to_number<double>(tokens[2]);
+      pt.Pu_x  = core::str::to_number<double>(tokens[3]);
+      pt.Pu_y  = core::str::to_number<double>(tokens[4]);
+      pt.Pu_z  = core::str::to_number<double>(tokens[5]);
+      pt.Pv_x  = core::str::to_number<double>(tokens[6]);
+      pt.Pv_y  = core::str::to_number<double>(tokens[7]);
+      pt.Pv_z  = core::str::to_number<double>(tokens[8]);
+      pt.Puv_x = core::str::to_number<double>(tokens[9]);
+      pt.Puv_y = core::str::to_number<double>(tokens[10]);
+      pt.Puv_z = core::str::to_number<double>(tokens[11]);
+      pt.u     = core::str::to_number<double>(tokens[12]);
+      pt.v     = core::str::to_number<double>(tokens[13]);
+
+      this->pts[i][j] = pt;
+    }
+
+     //! Prepares polynomial patches.
+    void ToBezierPatches(std::vector< t_ptr<t_bsurf> >& tiles) const
+    {
+      /*const int numUisos = this->nptsU;
+      const int numVisos = this->nptsV;
+
+      for ( int j = 0; j < numVisos - 1; ++j )
+      {
+        for ( int i = 0; i < numUisos - 1; ++i )
+        {
+          const t_pt& P1 = this->pts[j + numUisos*i]
+          const int serialIdx = Astra_SurfPointSize*(iPoint + (numUisos)*jPoint);
+        }
+      }*/
+      // TODO: NYI
+    }
+
+    //! Converts surface data to a B-spline surface.
+    t_ptr<t_bsurf> ToBSplineSurface() const
+    {
+      // TODO: NYI
+      return nullptr;
+    }
+
+    std::string                      name;     //!< Surface name.
+    int                              nptsU;    //!< Number of points in the U direction.
+    int                              nptsV;    //!< Number of points in the V direction.
+    std::vector< std::vector<t_pt> > pts;      //!< Surface points in a grid.
+    int                              ptSerial; //!< Serial index of a point.
+  };
+
   //! Checks if the passed line tokens represent a curve.
   bool IsCurve(const std::vector<std::string>& tokens,
                t_curveData&                    data)
   {
-    if ( tokens.size() != Astra_CurveArraySize )
+    if ( tokens.size() != Astra_ArrayHeaderSize )
       return false;
 
-    if ( tokens[1] != core::str::to_string<int>(Astra_CurveArraySize) )
+    if ( tokens[1] != core::str::to_string<int>(Astra_CurvePointSize) )
       return false;
 
     data = t_curveData();
     data.name = tokens[0];
     data.npts = core::str::extract_int(tokens[2]);
+    return true;
+  }
+
+  //! Checks if the passed line tokens represent a surface.
+  bool IsSurface(const std::vector<std::string>& tokens,
+                 t_surfData&                     data)
+  {
+    if ( tokens.size() != Astra_ArrayHeaderSize )
+      return false;
+
+    // <name> _14_ 2 61 42 0 0
+    if ( tokens[1] != core::str::to_string<int>(Astra_SurfPointSize) )
+      return false;
+
+    data = t_surfData();
+    data.name  = tokens[0];                         // _<name>_ 14  2   61  42 0 0
+    data.nptsU = core::str::extract_int(tokens[2]); //  <name>  14 _2_  61  42 0 0
+    data.nptsV = core::str::extract_int(tokens[3]); //  <name>  14  2  _61_ 42 0 0
+
+    // Reserve space for the network of poles.
+    for ( size_t row = 0; row < data.nptsU; ++row )
+    {
+      data.pts.push_back({});
+      data.pts.back().resize(data.nptsV);
+    }
+
     return true;
   }
 }
@@ -157,14 +265,22 @@ bool geom_ReadAstra::Perform(const std::string& filename)
     return false;
   }
 
+  /* =========================================================
+   *  Stage 1: read data from file, no conversion to geometry.
+   * ========================================================= */
+
   enum Mode
   {
     Mode_Scan = 1,
-    Mode_Curve
+    Mode_Curve,
+    Mode_Surface
   };
 
   std::vector<t_curveData> curveDs;
+  std::vector<t_surfData>  surfDs;
+  //
   t_curveData* pCurrentCurve = nullptr;
+  t_surfData*  pCurrentSurf  = nullptr;
 
   // Loop over the file.
   Mode mode = Mode_Scan;
@@ -184,8 +300,9 @@ bool geom_ReadAstra::Perform(const std::string& filename)
     if ( !tokens.size() )
       continue;
 
-    // Check if it's a curve.
+    // Check if it's a curve or a surface.
     t_curveData curveData;
+    t_surfData  surfData;
     //
     if ( ::IsCurve(tokens, curveData) )
     {
@@ -197,6 +314,20 @@ bool geom_ReadAstra::Perform(const std::string& filename)
       pCurrentCurve = &curveDs.back();
       continue;
     }
+    //
+    else if ( ::IsSurface(tokens, surfData) )
+    {
+      m_progress.SendLogMessage(MobiusInfo(Normal) << "Next surface is '%1' with %2 U points "
+                                                      "and %3 V points."
+                                                   << surfData.name
+                                                   << surfData.nptsU
+                                                   << surfData.nptsV);
+
+      mode = Mode_Surface;
+      surfDs.push_back(surfData);
+      pCurrentSurf = &surfDs.back();
+      continue;
+    }
 
     if ( mode == Mode_Curve )
     {
@@ -205,14 +336,26 @@ bool geom_ReadAstra::Perform(const std::string& filename)
       else
         mode = Mode_Scan;
     }
-
-    // TODO: NYI
-
+    else if ( mode == Mode_Surface )
+    {
+      if ( pCurrentSurf->pts.size() < pCurrentSurf->nptsU*pCurrentSurf->nptsV )
+        pCurrentSurf->AddPoint(tokens);
+      else
+        mode = Mode_Scan;
+    }
   } // Until EOF.
+
+  /* =========================================
+   *  Stage 2: construct geometric primitives.
+   * ========================================= */
 
   // Make curves.
   for ( auto& cds : curveDs )
     m_curves.push_back( cds.ToBSplineCurve() );
+
+  // Make surface.
+  for ( auto& sds : surfDs )
+    m_surfaces.push_back( sds.ToBSplineSurface() );
 
   FILE.close();
   return true;
